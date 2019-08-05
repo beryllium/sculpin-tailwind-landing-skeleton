dd9f7bab237b66ec3e6edf6787d71519e3750aaf
Kevin Boyd <kevin.boyd@gmail.com>
2019-08-05 13:04:26 -0700

    Make it so.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// directory where compiled assets will be stored
+    .setOutputPath('source/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    .copyFiles({
+        from: './source/assets/icons'
+    })
+    .addStyleEntry('css/app', './source/assets/css/app.scss')
+    .addEntry('js/app', './source/assets/js/app.js')
+    .enablePostCssLoader()
+    .disableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    .enableSassLoader(function (options) {}, {
+        resolveUrlLoader: false
+    })
+;
+
+module.exports = Encore.getWebpackConfig();
